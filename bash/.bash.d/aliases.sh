8b16db4e10a31fec0d61b262d6fb61e4e3a1b591
Rudy Hill <rudyghill@gmail.com>
2018-10-07 21:11:33 -0600

    added bahsrc

---
+++ bash/.bash.d/aliases.sh
@@ -0,0 +1,26 @@
+# User specific aliases and functions
+alias mail="offlineimap -a Gmail && neomutt"
+alias ls="ls --color=always"
+#vim
+alias sv="sudo vim"
+alias v="vim"
+#ranger
+alias f="ranger"
+#pacman install
+alias pi="sudo pacman -S"
+#pacman remove
+alias pr="sudo pacman -R"
+#pacman update
+alias pu="sudo pacman -Syu"
+#GNU Octave
+alias o="octave --no-gui --quiet"
+#GNU Emacs
+alias e="emacs -nw"
+#Neofetch
+alias n="clear && neofetch"
+#i3 config
+alias i3c="vim ~/.i3/config"
+#brightness
+alias brightness="xrandr --output eDP1 --brightness"
+#git
+alias g="git"
