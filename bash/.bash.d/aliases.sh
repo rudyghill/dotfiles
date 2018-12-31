29996aa5d3073388de8e1522d896ae52ed2e0752
Rudy Hill <rudyghill@gmail.com>
2018-12-31 13:59:28 -0700

    restructure

--- bash/.bash.d/aliases.sh
+++ bash/.bash.d/aliases.sh
@@ -1,21 +1,16 @@
 # User specific aliases and functions
 alias mail="offlineimap -a Gmail && neomutt"
 alias ls="ls --color=always"
-#vim
+#editor/vim
+alias e=$EDITOR
 alias sv="sudo vim"
 alias v="vim"
 #ranger
 alias f="ranger"
-#pacman install
-alias pi="sudo pacman -S"
-#pacman remove
-alias pr="sudo pacman -R"
-#pacman update
-alias pu="sudo pacman -Syu"
+#pacman
+alias pi="sudo pacman"
 #GNU Octave
 alias o="octave --no-gui --quiet"
-#GNU Emacs
-alias e="emacs -nw"
 #Neofetch
 alias n="clear && neofetch"
 #i3 config
@@ -24,3 +19,5 @@ alias i3c="vim ~/.config/i3/config"
 alias brightness="xrandr --output eDP1 --brightness"
 #git
 alias g="git"
+#rss/newsboat
+alias rss="newsboat"
